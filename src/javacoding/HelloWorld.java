e8de1cabcab5eab5e5bf18df0558a183c6cc02ab
Bhavyakrish <bhavyakrish2001@gmail.com>
2023-07-05 12:46:07 +0530

    basic change

--- src/javacoding/HelloWorld.java
+++ src/javacoding/HelloWorld.java
@@ -2,6 +2,6 @@ package javacoding;
 
 public class HelloWorld {
 	 public static void main(String[] args) {
-	        System.out.println("Hello, World!"); 
+	        System.out.println("Hello,Jspider!"); 
 	    }
 }
